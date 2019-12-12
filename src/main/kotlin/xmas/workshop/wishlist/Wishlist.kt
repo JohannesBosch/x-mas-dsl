5d09ba93b1e99b34c8401d44d7c440835f121c26
Johannes Bosch <Johannes.bosch@volkswagen.de>
2019-12-13 00:33:44 +0100

    Add desciption

--- src/main/kotlin/xmas/workshop/wishlist/Wishlist.kt
+++ src/main/kotlin/xmas/workshop/wishlist/Wishlist.kt
@@ -2,4 +2,18 @@ package xmas.workshop.wishlist
 
 fun main() {
     println("****** X-MAS Wish List ******")
+
+    //First Part
+    //TODO: Create a Letter class with a Title, a Address and a Wish List
+    //TODO: Create a Address class with a Name, a Street, a Number and a City
+    //TODO: Create a Wish class with a Title and a Description
+    //TODO: Create a method to print the Wish List
+    //TODO: Create a instance of the Wish List
+
+    //Second Part
+    //TODO: Create a function to crate the Wish List without receiver object and apply
+    //TODO: Add receiver object and remove its
+    //TODO: Use apply
+    //TODO: Create extension function for address and wishes
+    //TODO: Overload operator
 }
